3fbe3abe95311f01797b3a7904f38123d24c5c86
Eun-seo <58820215+Eun-seo@users.noreply.github.com>
2022-04-23 00:55:40 +0900

    feat:감정분석 fragment_main 에 감정분석 기능 추가

--- app/src/main/java/com/example/modutracker/fragment/MainFragment.kt
+++ app/src/main/java/com/example/modutracker/fragment/MainFragment.kt
@@ -1,66 +1,96 @@
 package com.example.modutracker.fragment
 
 
-
-
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.modutracker.R
+import com.example.modutracker.databinding.FragmentMainBinding
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
+import java.text.SimpleDateFormat
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
 
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
 
+        val textdiary = binding.editTextDiary
+        val btnAnalyze = binding.btnAnalyze
+        val textDate = binding.textDate
 
+        //날짜
+        val now = System.currentTimeMillis()
+        val date = SimpleDateFormat("MMMM dd일").format(now)
+        textDate.text = date
 
+        //감정 분석 버튼
+        btnAnalyze.setOnClickListener{
+            var diary = textdiary?.text.toString()
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            if(diary != null) {
+                AnalyzeEmotion(diary)
             }
+        }
+
+        return binding.root
     }
+
+    //감정 분석 - Clova Sentiment
+    fun AnalyzeEmotion(diarytext : String) {
+
+        val url = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze"
+
+        val json = JSONObject()
+        json.put("content", diarytext)
+
+        val requestBody = json.toString().toRequestBody("application/json;charset=utf-8".toMediaType())
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("X-NCP-APIGW-API-KEY-ID","r2g6zb12fz")
+            .addHeader("X-NCP-APIGW-API-KEY","mcYRLy2wvNaJfqN7NpMqDmHWHG3ReyZyKN3S4rJw")
+            .addHeader("Content-Type","application/json")
+            .post(requestBody)
+            .build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("요청", "Success")
+
+                var jsonObject = JSONObject(response.body?.string())
+                var sentiment = jsonObject.getJSONObject("document").get("sentiment") as String
+                var neutral = jsonObject.getJSONObject("document").getJSONObject("confidence").get("neutral") as Double
+                var positive = jsonObject.getJSONObject("document").getJSONObject("confidence").get("positive") as Double
+                var negative = jsonObject.getJSONObject("document").getJSONObject("confidence").get("negative") as Double
+
+                binding.textText.text = sentiment
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("요청", "Fail")
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
