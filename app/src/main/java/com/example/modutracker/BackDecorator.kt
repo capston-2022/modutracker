0909dddfe611c5359f4e1532ec1e54c694f42be0
Eun-seo <amydacom@gmail.com>
2022-05-23 17:38:17 +0900

    캘린더 배경 이미지 생성

--- app/src/main/java/com/example/modutracker/BackDecorator.kt
+++ app/src/main/java/com/example/modutracker/BackDecorator.kt
@@ -40,26 +40,74 @@ class BackDecorator (context : Activity, currentDay: CalendarDay) : DayViewDecor
         for(i in 0 until width){
             for(j in 0 until height){
                 //그림 크기
-                val imgWidth = 216.0
-                val imgHeight = 216.0
+                val margin = 40
+                val imgWidth = (width - margin * 2).toDouble()
+                val imgHeight = (height - margin * 2).toDouble()
 
                 //반지름
-                val radius = sqrt(imgWidth.pow(2) + imgHeight.pow(2)) / 4
+                val radius = imgWidth * sqrt(2.0) / (1 + sqrt(2.0)) / 2
 
-                //원
-                val sideX = (width/2 - 1 - i).toDouble()
-                val sideY = (height/2 - 1 - j).toDouble()
-                val distance = sideX.pow(2) + sideY.pow(2) //원의 중심과 점 사이의 거리의 제곱
-                //val radius = (width / 2).toDouble() - 20
+                //원1
+                val center1 = radius + margin //원의 중심
+
+                val sideX1 = (center1 - 1 - i)
+                val sideY1 = (center1 - 1 - j)
+                val distance1 = sideX1.pow(2) + sideY1.pow(2) //원의 중심과 점 사이의 거리의 제곱
                 var color = 0
-                if(distance <= (radius.pow(2))) //거리가 반지름의 길이보다 작으면(원 안에 있는 점이면)
+                if(distance1 <= (radius.pow(2))) //거리가 반지름의 길이보다 작으면(원 안에 있는 점이면)
                 {
                     color = Color.BLUE
                 }
                 else //거리가 반지름의 길이보다 크면(원 밖의 점이면)
                 {
-                    color = Color.alpha(Color.TRANSPARENT)
+                   // color = Color.alpha(Color.TRANSPARENT)
                 }
+
+                //원2
+                val centerX2 = imgWidth - radius + margin//원의 중심 x좌표
+                val centerY2 = radius + margin //원의 중심 y좌표
+
+                val sideX2 = (centerX2 - 1 - i)
+                val sideY2 = (centerY2 - 1 - j)
+                val distance2 = sideX2.pow(2) + sideY2.pow(2) //원의 중심과 점 사이의 거리의 제곱
+                if(distance2 <= (radius.pow(2))) //거리가 반지름의 길이보다 작으면(원 안에 있는 점이면)
+                {
+                    color = Color.MAGENTA
+                }
+                else //거리가 반지름의 길이보다 크면(원 밖의 점이면)
+                {
+                   // color = Color.alpha(Color.TRANSPARENT)
+                }
+
+                //원4
+                val centerX4 = imgWidth - radius + margin//원의 중심 x좌표
+                val centerY4 = imgWidth - radius + margin //원의 중심 y좌표
+
+                val sideX4 = (centerX4 - 1 - i)
+                val sideY4 = (centerY4 - 1 - j)
+                val distance4 = sideX4.pow(2) + sideY4.pow(2) //원의 중심과 점 사이의 거리의 제곱
+                if(distance4 <= (radius.pow(2))) //거리가 반지름의 길이보다 작으면(원 안에 있는 점이면)
+                {
+                    color = Color.GREEN
+                }
+
+                //원3
+                val centerX3 = radius + margin//원의 중심 x좌표
+                val centerY3 = imgWidth - radius + margin //원의 중심 y좌표
+
+                val sideX3 = (centerX3 - 1 - i)
+                val sideY3 = (centerY3 - 1 - j)
+                val distance3 = sideX3.pow(2) + sideY3.pow(2) //원의 중심과 점 사이의 거리의 제곱
+                if(distance3 <= (radius.pow(2))) //거리가 반지름의 길이보다 작으면(원 안에 있는 점이면)
+                {
+                    color = Color.YELLOW
+                }
+
+                if(distance1 <= (radius.pow(2)) && distance3 <= (radius.pow(2)))
+                {
+                    color = Color.BLUE
+                }
+
                 bitmap.setPixel(i,j,color)
             }
         }
